e4abf360ab739936f412b057c1efd4ed07001a65
Kai <31229627+KaiHaskell@users.noreply.github.com>
2020-01-24 12:48:26 -0500

    Added dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { applyMiddleware, createStore, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+//Styling
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
